4676cf04b400d51ba14e331e08fcd48fac4c88ea
Expertlance <info@expertlance.eu>
2026-07-07 13:38:08 +0300

    ...

--- webserver.nix
+++ webserver.nix
@@ -17,27 +17,27 @@
     };    
   };
   networking.firewall.allowedTCPPorts = [ 80 443 ];
-}
 
-let
-  phpWithIoncube = pkgs.php85.withExtensions (exts: [
-    exts.ioncube-loader
-    # add other extensions you need, e.g. exts.curl, exts.opcache, ...
-  ]);
-in
-{
-  #php
-  services.phpfpm = {
-    enable = true;
-    phpPackage = phpWithIoncube;
+  let
+    phpWithIoncube = pkgs.php85.withExtensions (exts: [
+      exts.ioncube-loader
+      # add other extensions you need, e.g. exts.curl, exts.opcache, ...
+    ]);
+  in
+  {
+    #php
+    services.phpfpm = {
+      enable = true;
+      phpPackage = phpWithIoncube;
 
-    phpOptions = ''
-      memory_limit = 256M
-      post_max_size = 20M
-      upload_max_filesize = 20M
-      date.timezone = "Europe/Copenhagen"
-      session.gc_maxlifetime = 21600
-      opcache.enable = 0
-    '';
-  };
+      phpOptions = ''
+        memory_limit = 256M
+        post_max_size = 20M
+        upload_max_filesize = 20M
+        date.timezone = "Europe/Copenhagen"
+        session.gc_maxlifetime = 21600
+        opcache.enable = 0
+      '';
+    };
+  }
 }
