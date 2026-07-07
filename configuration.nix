166b2bf6961ebc53b24add98cbdb35757d338c2c
Expertlance <info@expertlance.eu>
2026-07-07 13:44:43 +0300

    ...

--- configuration.nix
+++ configuration.nix
@@ -66,6 +66,8 @@
 	hdparm
 	htop
 	mc
+  php85
+  php85Extensions.ioncube-loader
 	pydf
 	tmux
 	vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
