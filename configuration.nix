7676a526ae5c4c32dbd3f9c108a572cfbfd3f273
Expertlance <info@expertlance.eu>
2026-07-07 21:37:27 +0300

    added tcpdump

--- configuration.nix
+++ configuration.nix
@@ -82,6 +82,7 @@
     php85Extensions.ioncube-loader
     psmisc
     pydf
+    tcpdump
     tmux
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
