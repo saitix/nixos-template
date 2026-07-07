e4100539abb9f1a04e423700689651b76498f5a6
Expertlance <info@expertlance.eu>
2026-07-07 13:39:37 +0300

    ...

--- webserver.nix
+++ webserver.nix
@@ -1,7 +1,13 @@
 { config, pkgs, ... }:
 
+let
+  phpWithIoncube = pkgs.php85.withExtensions (exts: [
+    exts.ioncube-loader
+    # add other extensions you need, e.g. exts.curl, exts.opcache, ...
+  ]);
+in
 {
-  #webserver
+  # webserver
   services.httpd = {
     enable = true;
     adminAddr = "admin@example.com";
@@ -14,30 +20,24 @@
           SetHandler "proxy:unix:/run/phpfpm/php-fpm.sock|fcgi://localhost/"
         </FilesMatch>
       '';
-    };    
+    };
   };
+
+  # open firewall
   networking.firewall.allowedTCPPorts = [ 80 443 ];
 
-  let
-    phpWithIoncube = pkgs.php85.withExtensions (exts: [
-      exts.ioncube-loader
-      # add other extensions you need, e.g. exts.curl, exts.opcache, ...
-    ]);
-  in
-  {
-    #php
-    services.phpfpm = {
-      enable = true;
-      phpPackage = phpWithIoncube;
+  # PHP-FPM with ionCube loader
+  services.phpfpm = {
+    enable = true;
+    phpPackage = phpWithIoncube;
 
-      phpOptions = ''
-        memory_limit = 256M
-        post_max_size = 20M
-        upload_max_filesize = 20M
-        date.timezone = "Europe/Copenhagen"
-        session.gc_maxlifetime = 21600
-        opcache.enable = 0
-      '';
-    };
-  }
+    phpOptions = ''
+      memory_limit = 256M
+      post_max_size = 20M
+      upload_max_filesize = 20M
+      date.timezone = "Europe/Copenhagen"
+      session.gc_maxlifetime = 21600
+      opcache.enable = 0
+    '';
+  };
 }
